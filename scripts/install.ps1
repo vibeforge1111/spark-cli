35c9727826268fe367ab72d2da0780969e9d1a8e
vibeforge1111 <vibeforge1111@gmail.com>
2026-05-11 20:58:28 +0400

    Point installers at registry-ref verifier CLI

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Prefix = "$HOME\.spark",
     [string]$Source = "https://github.com/vibeforge1111/spark-cli",
-    [string]$Ref = "1cafec0bea8fc4c2d0ba9fc439cf35ab5cd6b4ad",
+    [string]$Ref = "47d36350fa453261609119c5ae788f4d5b6d6665",
     [string]$NodeVersion = "22.18.0",
     [string]$PythonVersion = "3.11",
     [string]$UvVersion = "0.11.7",
