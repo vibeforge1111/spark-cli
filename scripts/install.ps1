409dbb1119046e37d91d5fed848252116aea9de5
Meta Alchemist <alchemistab@Clawds-MacBook-Air.local>
2026-06-27 12:12:29 +0400

    chore(installer): default public installer to r29

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Prefix = "$HOME\.spark",
     [string]$Source = "https://github.com/vibeforge1111/spark-cli",
-    [string]$Ref = "spark-cli-public-installer-2026-06-22-r28",
+    [string]$Ref = "spark-cli-public-installer-2026-06-26-r29",
     [string]$NodeVersion = "22.18.0",
     [string]$PythonVersion = "3.11",
     [string]$UvVersion = "0.11.7",
@@ -36,7 +36,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$SparkCliReleaseName = "spark-cli-public-installer-2026-06-22-r28"
+$SparkCliReleaseName = "spark-cli-public-installer-2026-06-26-r29"
 $RefWasProvided = $PSBoundParameters.ContainsKey("Ref")
 $Script:InstallLockDir = ""
 $Script:PythonExe = ""
